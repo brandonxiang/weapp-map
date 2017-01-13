cfa789d188d4190dc6bcdc01f9c35faf8f7e256d
brandonxiang <1542453460@qq.com>
2017-01-13 14:37:32 +0800

    baidu map weather

--- pages/amap/amap.js
+++ pages/amap/amap.js
@@ -6,21 +6,35 @@ Page({
     markers: [],
     latitude: '',
     longitude: '',
-    textData: {}
+    placeData: {},
+
+    AMap:{}
   },
-  makertap: function(e) {
+  makertap: function (e) {
     var id = e.markerId;
     var that = this;
-    that.showMarkerInfo(markersData,id);
-    that.changeMarkerColor(markersData,id);
+    that.showMarkerInfo(markersData, id);
+    that.changeMarkerColor(markersData, id);
   },
-  onLoad: function() {
+  onLoad: function () {
     var that = this;
-    var myAmapFun = new amapFile.AMapWX({key:'ea889682997a181fd6678d9aaf23693a'});
-    myAmapFun.getPoiAround({
+    this.setData({AMap:new amapFile.AMapWX({ key: 'ea889682997a181fd6678d9aaf23693a' })}) ;
+
+    wx.getLocation({
+      type: 'wgs84',
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        that.setData({ latitude: latitude, longitude: longitude })
+      }
+    })
+  },
+  onSearch: function () {
+    var that = this;
+    this.data.AMap.getPoiAround({
       iconPathSelected: '../../image/marker_red.png', //如：..­/..­/img/marker_checked.png
       iconPath: '../../image/marker_yellow.png', //如：..­/..­/img/marker.png
-      success: function(data){
+      success: function (data) {
         markersData = data.markers;
         that.setData({
           markers: markersData
@@ -31,29 +45,35 @@ Page({
         that.setData({
           longitude: markersData[0].longitude
         });
-        that.showMarkerInfo(markersData,0);
+        that.showMarkerInfo(markersData, 0);
       },
-      fail: function(info){
-        wx.showModal({title:info.errMsg})
+      fail: function (info) {
+        wx.showModal({ title: info.errMsg })
       }
     })
   },
-  showMarkerInfo: function(data,i){
+  weather: function () {
+    wx.navigateTo({
+      url: '../amapweather/amapweather'
+    })
+  },
+
+  showMarkerInfo: function (data, i) {
     var that = this;
     that.setData({
-      textData: {
-        name: data[i].name,
-        desc: data[i].address
+      placeData: {
+        title: data[i].name,
+        address: data[i].address
       }
     });
   },
-  changeMarkerColor: function(data,i){
+  changeMarkerColor: function (data, i) {
     var that = this;
     var markers = [];
-    for(var j = 0; j < data.length; j++){
-      if(j==i){
+    for (var j = 0; j < data.length; j++) {
+      if (j == i) {
         data[j].iconPath = "../../image/marker_red.png"; //如：..­/..­/img/marker_checked.png
-      }else{
+      } else {
         data[j].iconPath = "../../image/marker_yellow.png"; //如：..­/..­/img/marker.png
       }
       markers.push(data[j]);
@@ -62,5 +82,4 @@ Page({
       markers: markers
     });
   }
- 
 })
