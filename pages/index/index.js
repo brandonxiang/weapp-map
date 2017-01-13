4613376241d8ce1aa59d46e758d1f6a8ddb1a080
brandonxiang <1542453460@qq.com>
2017-01-13 11:27:32 +0800

    amap and baidu map

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,3 @@
-import {getDailyWeather,getNowWeather} from '../../utils/service'
-import {KEY} from '../../utils/key.js'
-
-var city ='shenzhen'
-var unit ='c'
-var lang = 'zh-Hans'
-
 var app = getApp()
 Page({
   data: {
@@ -18,43 +11,6 @@ Page({
     // })
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
-
-    app.getUserInfo(function(userInfo){
-
-      that.setData({
-        userInfo:userInfo
-      })
-      that.update()
-    })
-
-    console.log(KEY)
-
-    getDailyWeather({
-      data:{
-        key:KEY,
-        location:city,
-        language: lang,
-        unit:unit,
-        start:0,
-        days:3
-      },
-      success:(res)=>{
-        console.log(res)
-      }
-    })
-
-    getNowWeather({
-      data:{
-        key:KEY,
-        location:city,
-        language:lang,
-        unit:unit,
-      },
-      success: (res)=>{
-        console.log(res)
-      }
-    })
+   
   }
 })
